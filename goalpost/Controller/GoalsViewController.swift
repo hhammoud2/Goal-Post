54049a450ac100411fa6da52bb71679f4e0a1cca
Hammoud Hammoud <hammoudhammoud@TJs-Mac-mini.local>
2017-11-22 13:18:10 -0800

    Prototype cell added, dimensions not working correctly

--- goalpost/Controller/GoalsViewController.swift
+++ goalpost/Controller/GoalsViewController.swift
@@ -32,9 +32,74 @@ class GoalsViewController: UIViewController {
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.isHidden = true
         tableView.rowHeight = 70
-        let prototypeCell = UITableViewCell(frame: CGRect(x: 0, y: 0, width: screenSize.width, height: tableView.rowHeight))
-        tableView.addSubview(prototypeCell)
         return tableView
+    }()
+    
+    let goalTableCell : UITableViewCell = {
+        let prototypeCell = UITableViewCell(frame: CGRect(x: screenSize.width/2, y: screenSize.height/2, width: screenSize.width, height: 100))
+        prototypeCell.backgroundColor = .red
+        print("Prototype bounds: \(prototypeCell.bounds)")
+        
+        //Text label: "Goal:"
+        let goalTextLabel = UILabel(frame: CGRect(x: 10, y: 10, width: 50, height: 20))
+        
+        //
+        goalTextLabel.backgroundColor = .white
+        //
+        
+        goalTextLabel.text = "Goal:"
+        goalTextLabel.font = UIFont(name: "Georgia-Bold", size: 18)
+        prototypeCell.addSubview(goalTextLabel)
+        goalTextLabel.pin.top().margin(5).topLeft()
+        
+        //Text label which describes the goal
+        let goalDescriptionLabel = UILabel(frame: CGRect(x: 11, y: 11, width: 50, height: 20))
+        goalDescriptionLabel.text = "Example goal inserted here, will populate with user data"
+        goalDescriptionLabel.font = UIFont(name: "Georgia-Bold", size: 18)
+        goalDescriptionLabel.adjustsFontSizeToFitWidth = true
+        goalDescriptionLabel.minimumScaleFactor = 0.5
+        goalDescriptionLabel.numberOfLines = 2
+        goalDescriptionLabel.lineBreakMode = .byWordWrapping
+        prototypeCell.addSubview(goalDescriptionLabel)
+
+        //Text label: "Type:"
+        let typeTextLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 50, height: 20))
+        
+        //
+        typeTextLabel.backgroundColor = .blue
+        //
+        
+        typeTextLabel.text = "Type:"
+        typeTextLabel.font = UIFont(name: "Georgia", size: 14)
+        prototypeCell.addSubview(typeTextLabel)
+        typeTextLabel.pin.below(of: goalTextLabel, aligned: .left).bottom().marginRight(5)
+        
+        //Text label which denotes whether the goal is short- or long-term
+        let typeDescriptionLabel = UILabel(frame: CGRect(x: 0, y: 0, width: screenSize.width - 100, height: 25))
+        typeDescriptionLabel.text = "Short-Term"
+        typeDescriptionLabel.font = UIFont(name: "Georgia", size: 14)
+        prototypeCell.addSubview(typeDescriptionLabel)
+        typeDescriptionLabel.pin.after(of: typeTextLabel).bottom()
+        
+        //Text label denoting how many weeks/days/hours left until goal is finished as a whole number
+        let timeRemainingLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 40, height: 60))
+        
+        //
+        timeRemainingLabel.backgroundColor = .blue
+        //
+        
+        timeRemainingLabel.text = "9"
+        timeRemainingLabel.font = UIFont(name: "Georgia-Bold", size: 24)
+        //Same color as the title background
+        timeRemainingLabel.textColor = UIColor(red: 0x6D/255, green: 0xBC/255, blue: 0x63/255, alpha: 1.0)
+        timeRemainingLabel.textAlignment = .center
+        timeRemainingLabel.adjustsFontSizeToFitWidth = true
+        timeRemainingLabel.minimumScaleFactor = 0.5
+        prototypeCell.addSubview(timeRemainingLabel)
+        timeRemainingLabel.pin.right().vCenter()
+        goalDescriptionLabel.pin.topLeft(to: goalTextLabel.anchor.topRight)
+        typeDescriptionLabel.pin.topLeft(to: typeTextLabel.anchor.topRight)
+        return prototypeCell
     }()
     
     // MARK: Lifecycle
@@ -59,6 +124,12 @@ class GoalsViewController: UIViewController {
         titleView.subviews[0].pin.topRight(to: titleView.anchor.center).bottom(to: titleView.edge.bottom).marginHorizontal(0).marginBottom(12)
         titleView.subviews[1].pin.topLeft(to: titleView.anchor.center).bottom(to: titleView.edge.bottom).marginHorizontal(0).marginBottom(12)
         titleView.subviews[2].pin.right().marginHorizontal(8).vCenter(to: titleView.subviews[0].edge.vCenter).marginBottom(2)
+        
+        //Adding prototypecell
+        print("Adding prototype cell")
+        self.view.addSubview(goalTableCell)
+        goalTableCell.pin.vCenter().hCenter()
+
     }
 
     // MARK: - Setup
